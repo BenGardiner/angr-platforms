7756c26c9ae495e0594267c6baa87205c5542bff
Ben Gardiner <ben.l.gardiner@gmail.com>
2018-11-25 23:27:31 -0500

    use find_packages() to get all submodules installed

--- setup.py
+++ setup.py
@@ -1,10 +1,11 @@
+from setuptools import find_packages
 from distutils.core import setup
 
 setup(
     name='angr_platforms',
     version='0.1',
     description='A collection of extra platforms for angr',
-    packages=['angr_platforms'],
+    packages=find_packages(),
     install_requires=[
         'angr',
         'cle',
